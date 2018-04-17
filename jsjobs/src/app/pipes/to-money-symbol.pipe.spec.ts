1ea149d3bdf5a3a417c44d1b8d72e5aa94a1c8bd
Léa BARBEAU <leabarbeau@yahoo.fr>
2018-04-17 13:55:20 +0200

    suppression jsjobs

--- jsjobs/src/app/pipes/to-money-symbol.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToMoneySymbolPipe } from './to-money-symbol.pipe';
-
-describe('ToMoneySymbolPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToMoneySymbolPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- jsjobs/src/app/pipes/to-short-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToShortDatePipe } from './to-short-date.pipe';
-
-describe('ToShortDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToShortDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
